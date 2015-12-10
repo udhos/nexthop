b3cad5d0cca77f181235de1ae8eb48b898075640
Everton Marques <everton.marques@gmail.com>
2015-12-10 17:30:20 -0200

    Move show conf to lib.

---
+++ src/command/show_conf.go
@@ -0,0 +1,72 @@
+package command
+
+import (
+	"fmt"
+	"strings"
+)
+
+func ShowConf(root *ConfNode, node *CmdNode, c CmdClient, lineMode bool) {
+	for _, n := range root.Children {
+		if lineMode {
+			showConfLine(n, 0, c)
+		} else {
+			showConf(n, 0, 1, c, false)
+		}
+	}
+}
+
+func showConfLine(node *ConfNode, depth int, c CmdClient) {
+
+	// show node values
+	for _, v := range node.Value {
+		c.Sendln(fmt.Sprintf("%s %s", node.Path, v))
+	}
+
+	for _, n := range node.Children {
+		showConfLine(n, depth+1, c)
+	}
+}
+
+func showConf(node *ConfNode, depth, valueDepth int, c CmdClient, hasSibling bool) {
+
+	var ident string
+	nodeIdent := strings.Repeat(" ", 2*depth)
+	last := LastToken(node.Path)
+
+	childrenCount := len(node.Children)
+	valueCount := len(node.Value)
+	fanout := childrenCount + valueCount
+
+	// show path
+	if hasSibling {
+		ident = nodeIdent
+	} else {
+		ident = ""
+	}
+	var identValue bool // need to increment identation for value
+	if fanout > 1 {
+		c.Sendln(fmt.Sprintf("%s%s", ident, last))
+		identValue = true
+	} else {
+		c.Send(fmt.Sprintf("%s%s ", ident, last))
+		identValue = false
+	}
+
+	// show value
+	if valueCount == 1 {
+		c.Sendln(fmt.Sprintf("%s", node.Value[0]))
+	} else {
+		tab := strings.Repeat(" ", 2*valueDepth)
+		for _, v := range node.Value {
+			c.Sendln(fmt.Sprintf("%s%s", tab, v))
+		}
+	}
+
+	if identValue {
+		valueDepth++
+	}
+
+	for _, n := range node.Children {
+		showConf(n, depth+1, valueDepth, c, childrenCount > 1)
+	}
+}
--- src/rib/cmd_table.go
+++ src/rib/cmd_table.go
@@ -238,69 +238,7 @@ func showConfig(root *command.ConfNode, node *command.CmdNode, line string, c co
 	fields := strings.Fields(line)
 	lineMode := len(fields) > 2 && strings.HasPrefix("line-mode", fields[2])
 	c.Sendln(head)
-	for _, n := range root.Children {
-		if lineMode {
-			showConfLine(n, 0, c)
-		} else {
-			showConf(n, 0, 1, c, false)
-		}
-	}
-}
-
-func showConfLine(node *command.ConfNode, depth int, c command.CmdClient) {
-
-	// show node values
-	for _, v := range node.Value {
-		c.Sendln(fmt.Sprintf("%s %s", node.Path, v))
-	}
-
-	for _, n := range node.Children {
-		showConfLine(n, depth+1, c)
-	}
-}
-
-func showConf(node *command.ConfNode, depth, valueDepth int, c command.CmdClient, hasSibling bool) {
-
-	var ident string
-	nodeIdent := strings.Repeat(" ", 2*depth)
-	last := command.LastToken(node.Path)
-
-	childrenCount := len(node.Children)
-	valueCount := len(node.Value)
-	fanout := childrenCount + valueCount
-
-	// show path
-	if hasSibling {
-		ident = nodeIdent
-	} else {
-		ident = ""
-	}
-	var identValue bool // need to increment identation for value
-	if fanout > 1 {
-		c.Sendln(fmt.Sprintf("%s%s", ident, last))
-		identValue = true
-	} else {
-		c.Send(fmt.Sprintf("%s%s ", ident, last))
-		identValue = false
-	}
-
-	// show value
-	if valueCount == 1 {
-		c.Sendln(fmt.Sprintf("%s", node.Value[0]))
-	} else {
-		tab := strings.Repeat(" ", 2*valueDepth)
-		for _, v := range node.Value {
-			c.Sendln(fmt.Sprintf("%s%s", tab, v))
-		}
-	}
-
-	if identValue {
-		valueDepth++
-	}
-
-	for _, n := range node.Children {
-		showConf(n, depth+1, valueDepth, c, childrenCount > 1)
-	}
+	command.ShowConf(root, node, c, lineMode)
 }
 
 func cmdShowIPAddr(ctx command.ConfContext, node *command.CmdNode, line string, c command.CmdClient) {
