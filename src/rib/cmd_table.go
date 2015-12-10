c44ab5887984661b52aa58dd93958c38657b0511
Everton Marques <everton.marques@gmail.com>
2015-12-10 16:49:36 -0200

    Fix compact configuration view.

--- src/rib/cmd_table.go
+++ src/rib/cmd_table.go
@@ -238,56 +238,68 @@ func showConfig(root *command.ConfNode, node *command.CmdNode, line string, c co
 	fields := strings.Fields(line)
 	lineMode := len(fields) > 2 && strings.HasPrefix("line-mode", fields[2])
 	c.Sendln(head)
-	//showConf(root, 0, c, lineMode, false)
 	for _, n := range root.Children {
-		showConf(n, 0, c, lineMode, false)
+		if lineMode {
+			showConfLine(n, 0, c)
+		} else {
+			showConf(n, 0, 1, c, false)
+		}
 	}
 }
 
-func showConf(node *command.ConfNode, depth int, c command.CmdClient, lineMode, hasSibling bool) {
-	var ident string
-	if hasSibling {
-		ident = strings.Repeat(" ", 2*depth)
-	} else {
-		ident = ""
-	}
-
-	var last string
-	if lineMode {
-		last = node.Path
-	} else {
-		last = command.LastToken(node.Path)
-	}
-
-	moreThanOneChild := len(node.Children) > 1
-
-	// show node path
-	if !lineMode {
-		if moreThanOneChild {
-			c.Sendln(fmt.Sprintf("%s%s", ident, last))
-		} else {
-			c.Send(fmt.Sprintf("%s%s ", ident, last))
-		}
-	}
+func showConfLine(node *command.ConfNode, depth int, c command.CmdClient) {
 
 	// show node values
-
-	if !lineMode && len(node.Value) == 1 {
-		c.Sendln(fmt.Sprintf("%s %s", last, node.Value[0]))
-	} else {
-
-		for _, v := range node.Value {
-			if lineMode {
-				c.Sendln(fmt.Sprintf("%s %s", last, v))
-			} else {
-				c.Sendln(fmt.Sprintf("%s %s", ident, v))
-			}
-		}
-
+	for _, v := range node.Value {
+		c.Sendln(fmt.Sprintf("%s %s", node.Path, v))
 	}
 
 	for _, n := range node.Children {
-		showConf(n, depth+1, c, lineMode, moreThanOneChild)
+		showConfLine(n, depth+1, c)
+	}
+}
+
+func showConf(node *command.ConfNode, depth, valueDepth int, c command.CmdClient, hasSibling bool) {
+
+	var ident string
+	nodeIdent := strings.Repeat(" ", 2*depth)
+	last := command.LastToken(node.Path)
+
+	childrenCount := len(node.Children)
+	valueCount := len(node.Value)
+	fanout := childrenCount + valueCount
+
+	// show path
+	if hasSibling {
+		ident = nodeIdent
+	} else {
+		ident = ""
+	}
+	var identValue bool // need to increment identation for value
+	if fanout > 1 {
+		c.Sendln(fmt.Sprintf("%s%s", ident, last))
+		identValue = true
+	} else {
+		c.Send(fmt.Sprintf("%s%s ", ident, last))
+		identValue = false
+	}
+
+	// show value
+	if valueCount == 1 {
+		c.Sendln(fmt.Sprintf("%s", node.Value[0]))
+	} else {
+		tab := strings.Repeat(" ", 2*valueDepth)
+		for _, v := range node.Value {
+			c.Sendln(fmt.Sprintf("%s%s", tab, v))
+		}
+	}
+
+	if identValue {
+		valueDepth++
+	}
+
+	for _, n := range node.Children {
+		showConf(n, depth+1, valueDepth, c, childrenCount > 1)
 	}
 }
 
