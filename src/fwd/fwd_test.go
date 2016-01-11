f3c76d8f1aa6078da95108d55593eb03c2c6e4f6
Everton Marques <everton.marques@gmail.com>
2016-01-11 17:41:57 -0200

    Missing file.

---
+++ src/fwd/fwd_test.go
@@ -0,0 +1,27 @@
+package fwd
+
+import (
+	"net"
+	"testing"
+)
+
+func TestIntersect(t *testing.T) {
+	_, net1, _ := net.ParseCIDR("1.1.1.1/24")
+	_, net2, _ := net.ParseCIDR("1.1.0.2/16")
+	_, net3, _ := net.ParseCIDR("1.1.1.3/25")
+	_, net4, _ := net.ParseCIDR("1.2.0.4/16")
+
+	test(t, net1, net2, true)
+	test(t, net2, net1, true)
+	test(t, net1, net3, true)
+	test(t, net3, net1, true)
+	test(t, net1, net4, false)
+	test(t, net4, net1, false)
+}
+
+func test(t *testing.T, n1, n2 *net.IPNet, expected bool) {
+	result := intersect(n1, n2)
+	if result != expected {
+		t.Errorf("intersect(%v,%v)=%v expected=%v", n1, n2, result, expected)
+	}
+}
