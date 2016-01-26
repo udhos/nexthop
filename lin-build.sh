ba6499238d4a5d1eefa97d24467e47642ad563db
Everton Marques <everton.marques@gmail.com>
2016-01-26 19:49:10 -0200

    Linux support.

---
+++ lin-build.sh
@@ -0,0 +1,17 @@
+# !/bin/bash
+#
+# lin-build
+
+DEVEL=/tmp/devel
+NEXTHOP=$DEVEL/nexthop
+GOPATH=$NEXTHOP
+
+go tool fix $NEXTHOP/src
+go tool vet $NEXTHOP/src/sample $NEXTHOP/src/rib-old $NEXTHOP/src/rib $NEXTHOP/src/cli $NEXTHOP/src/rip $NEXTHOP/src/telnet $NEXTHOP/src/command $NEXTHOP/src/fwd
+gofmt -s -w $NEXTHOP/src
+
+go install rib-old rib rip
+
+go test command cli
+
+# eof
---
+++ lin-gitbash-clone.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+export DEVEL=/tmp/devel
+export NEXTHOP=$DEVEL/nexthop
+export GOPATH=$NEXTHOP
+
+[ -d $DEVEL ] || mkdir -p $DEVEL
+cd $DEVEL
+
+git clone https://github.com/udhos/nexthop
+
+go_get () {
+	local i=$1
+	echo go get $i
+	go get $i
+}
+
+#go_get code.google.com/p/go.net/ipv4
+#go_get golang.org/x/net/ipv4
