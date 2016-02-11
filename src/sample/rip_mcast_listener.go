e2f3bf97d4fe25f7d1925b36ef30e407bc33645f
Everton Marques <everton.marques@gmail.com>
2016-02-11 13:06:50 -0200

    Command line arguments for sample rip multicast listener.

--- src/sample/rip_mcast_listener.go
+++ src/sample/rip_mcast_listener.go
@@ -4,104 +4,79 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
 
 	"golang.org/x/net/ipv4"
 )
 
 func main() {
-	if err := interfaceAdd("eth2"); err != nil {
-		log.Printf("main: error: %v", err)
+	if len(os.Args) != 4 {
+		fmt.Printf("usage:   %s interface protocol address:port\n", os.Args[0])
+		fmt.Printf("example: %s eth2      udp      1.0.0.2:520\n", os.Args[0])
+		return
 	}
 
-	log.Printf("main: waiting forever")
-	<-make(chan int)
+	ifname := os.Args[1]
+	proto := os.Args[2]
+	addrPort := os.Args[3]
+
+	if err := multicastRead(ifname, proto, addrPort); err != nil {
+		log.Printf("main: error: %v", err)
+	}
 }
 
-func interfaceAdd(s string) error {
+func multicastRead(ifname, proto, addrPort string) error {
 
-	iface, err1 := net.InterfaceByName(s)
+	iface, err1 := net.InterfaceByName(ifname)
 	if err1 != nil {
 		return err1
 	}
 
-	addrList, err2 := iface.Addrs()
-	if err2 != nil {
-		return err2
-	}
-
-	for _, a := range addrList {
-		addr, _, err3 := net.ParseCIDR(a.String())
-		if err3 != nil {
-			log.Printf("interfaceAdd: parse CIDR error for '%s' on '%s': %v", addr, s, err3)
-			continue
-		}
-		if err := join(iface, addr); err != nil {
-			log.Printf("interfaceAdd: join error for '%s' on '%s': %v", addr, s, err)
-		}
-	}
-
-	return nil
-}
-
-func join(iface *net.Interface, addr net.IP) error {
-	proto := "udp"
-	var a string
-	if addr.To4() == nil {
-		// IPv6
-		a = fmt.Sprintf("[%s]", addr.String())
-	} else {
-		// IPv4
-		a = addr.String()
-	}
-
-	hostPort := fmt.Sprintf("%s:520", a) // rip multicast port
-
-	// open socket (connection)
-	conn, err2 := net.ListenPacket(proto, hostPort)
-	if err2 != nil {
-		return fmt.Errorf("join: %s/%s listen error: %v", proto, hostPort, err2)
+	// open/bind socket
+	conn, err3 := net.ListenPacket(proto, addrPort)
+	if err3 != nil {
+		return fmt.Errorf("join: %s/%s listen error: %v", proto, addrPort, err3)
 	}
 
 	// join multicast address
-	pc := ipv4.NewPacketConn(conn)
-	if err := pc.JoinGroup(iface, &net.UDPAddr{IP: net.IPv4(224, 0, 0, 9)}); err != nil {
+	p := ipv4.NewPacketConn(conn)
+	if err := p.JoinGroup(iface, &net.UDPAddr{IP: net.IPv4(224, 0, 0, 9)}); err != nil {
 		conn.Close()
 		return fmt.Errorf("join: join error: %v", err)
 	}
 
-	if err := pc.SetMulticastInterface(iface); err != nil {
-		log.Printf("join: %s SetMulticastInterface(%s) error: %v", a, iface.Name, err)
+	// is this needed for receive?
+	if err := p.SetMulticastInterface(iface); err != nil {
+		log.Printf("join: %s SetMulticastInterface(%s) error: %v", addrPort, iface.Name, err)
 	}
 
 	{
-		ifi, err := pc.MulticastInterface()
+		ifi, err := p.MulticastInterface()
 		if err != nil {
-			log.Printf("join: %s %s multicastInterface error: %v", iface.Name, a, err)
+			log.Printf("join: %s %s multicastInterface error: %v", iface.Name, addrPort, err)
 		} else {
 			if ifi == nil {
-				log.Printf("join: %s %s multicastInterface=nil", iface.Name, a)
+				log.Printf("join: %s %s multicastInterface=nil", iface.Name, addrPort)
 			} else {
-				log.Printf("join: %s %s multicastInterface=%s", iface.Name, a, ifi.Name)
+				log.Printf("join: %s %s multicastInterface=%s", iface.Name, addrPort, ifi.Name)
 			}
 		}
 	}
 
 	// request control messages
-	/*
-		if err := pc.SetControlMessage(ipv4.FlagTTL|ipv4.FlagSrc|ipv4.FlagDst|ipv4.FlagInterface, true); err != nil {
-			// warning only
-			log.Printf("join: control message flags error: %v", err)
-		}
-	*/
+	if err := p.SetControlMessage(ipv4.FlagTTL|ipv4.FlagSrc|ipv4.FlagDst|ipv4.FlagInterface, true); err != nil {
+		// warning only
+		log.Printf("join: control message flags error: %v", err)
+	}
 
-	go udpReader(pc, iface.Name, addr.String())
+	udpReader(p, iface.Name, addrPort)
 
 	return nil
 }
 
-func udpReader(c *ipv4.PacketConn, ifname, ifaddr string) {
+func udpReader(c *ipv4.PacketConn, ifname, hostPort string) {
 
-	log.Printf("udpReader: reading from '%s' on '%s'", ifaddr, ifname)
+	log.Printf("udpReader: reading multicast from '%s' (hostPort: '%s')", ifname, hostPort)
 
 	defer c.Close()
 
@@ -113,10 +88,6 @@ func udpReader(c *ipv4.PacketConn, ifname, ifaddr string) {
 			log.Printf("udpReader: ReadFrom: error %v", err)
 			break
 		}
-
-		// make a copy because we will overwrite buf
-		b := make([]byte, n)
-		copy(b, buf)
 
 		log.Printf("udpReader: recv %d bytes from %s to %s on %s", n, cm.Src, cm.Dst, ifname)
 	}
