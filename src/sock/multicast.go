d2a9cd3ee938bd09d9d78b969dd89971f100c287
Everton Marques <everton.marques@gmail.com>
2016-03-07 21:24:48 -0300

    Fix spelling.

--- src/sock/multicast.go
+++ src/sock/multicast.go
@@ -25,7 +25,7 @@ func MulticastListener(port int, ifname string) (*MulticastSock, error) {
 	}
 	if err := syscall.SetsockoptString(s, syscall.SOL_SOCKET, syscall.SO_BINDTODEVICE, ifname); err != nil {
 		syscall.Close(s)
-		return nil, fmt.Errorf("MulticastListener: could bind to device socket(port=%d,ifname=%s): %v", port, ifname, err)
+		return nil, fmt.Errorf("MulticastListener: could not bind to device socket(port=%d,ifname=%s): %v", port, ifname, err)
 	}
 
 	bindAddr := net.IP(net.IPv4(0, 0, 0, 0))
@@ -34,14 +34,14 @@ func MulticastListener(port int, ifname string) (*MulticastSock, error) {
 
 	if err := syscall.Bind(s, &lsa); err != nil {
 		syscall.Close(s)
-		return nil, fmt.Errorf("MulticastListener: could bind socket to address %v,%d: %v", bindAddr, port, err)
+		return nil, fmt.Errorf("MulticastListener: could not bind socket to address %v,%d: %v", bindAddr, port, err)
 	}
 	f := os.NewFile(uintptr(s), "")
 	c, err2 := net.FilePacketConn(f)
 	f.Close()
 	if err2 != nil {
 		syscall.Close(s)
-		return nil, fmt.Errorf("MulticastListener: could get packet connection for socket(port=%d,ifname=%s): %v", port, ifname, err2)
+		return nil, fmt.Errorf("MulticastListener: could not get packet connection for socket(port=%d,ifname=%s): %v", port, ifname, err2)
 	}
 	u := c.(*net.UDPConn)
 	p := ipv4.NewPacketConn(c)
@@ -56,11 +56,11 @@ func MulticastListener(port int, ifname string) (*MulticastSock, error) {
 func Join(sock *MulticastSock, group net.IP, ifname string) error {
 	ifi, err1 := net.InterfaceByName(ifname)
 	if err1 != nil {
-		return fmt.Errorf("Join: could get find interface %s: %v", ifname, err1)
+		return fmt.Errorf("Join: could not find interface %s: %v", ifname, err1)
 	}
 
 	if err := sock.P.JoinGroup(ifi, &net.UDPAddr{IP: group}); err != nil {
-		return fmt.Errorf("Join: could get join group %v on interface %s: %v", group, ifname, err)
+		return fmt.Errorf("Join: could not join group %v on interface %s: %v", group, ifname, err)
 	}
 
 	return nil
@@ -68,7 +68,7 @@ func Join(sock *MulticastSock, group net.IP, ifname string) error {
 
 func Leave(sock *MulticastSock, group net.IP, ifi *net.Interface) error {
 	if err := sock.P.LeaveGroup(ifi, &net.UDPAddr{IP: group}); err != nil {
-		return fmt.Errorf("Leave: could get leave group %v on interface %s: %v", group, ifi.Name, err)
+		return fmt.Errorf("Leave: could not leave group %v on interface %s: %v", group, ifi.Name, err)
 	}
 
 	return nil
